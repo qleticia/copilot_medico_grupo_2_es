e45de43983a2f49f65f3840d1ba1d404ec56a282
Lucas Rafael Leite <lucas.rleite@upe.br>
2025-10-17 11:22:04 -0300

    alterando o modelo do gemini para um modelo válido

--- API/backend/gemini_connection.py
+++ API/backend/gemini_connection.py
@@ -24,7 +24,7 @@ except FileNotFoundError as e:
     raise
 
 # Configurações do Modelo
-MODEL_NAME = "gemini-1.5-flash-latest" # Recomendo 'gemini-1.5-flash-latest' ou 'gemini-1.5-pro-latest'
+MODEL_NAME = "gemini-flash-latest" # Recomendo 'gemini-1.5-flash-latest' ou 'gemini-1.5-pro-latest'
 SAFETY_SETTINGS = {
     HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
